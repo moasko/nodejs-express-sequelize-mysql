d84ca5c972d2725edb4b11b6d20a0d7702fec81b
moasko.dev <42101671+moasko@users.noreply.github.com>
2022-08-20 17:29:04 +0000

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,9 @@
+const production = true
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "moato2",
+  HOST: production ? 'us-cdbr-east-06.cleardb.net' : 'localhost',
+  USER: production ? 'b867b26fa345c5' : 'root',
+  PASSWORD: production ? 'bc2ce362' : '',
+  DB: production?"heroku_7d2703ae6d7a5da":"moato2",
   dialect: "mysql",
   pool: {
     max: 5,
@@ -11,3 +12,6 @@ module.exports = {
     idle: 10000
   }
 };
+
+
+//mysql://b867b26fa345c5:bc2ce362@us-cdbr-east-06.cleardb.net/heroku_7d2703ae6d7a5da?reconnect=true
